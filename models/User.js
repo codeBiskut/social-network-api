465f44d9119cba2573b97b11f876fb0482ab624b
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-10-24 13:16:18 -0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a new user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'Username is required',
+      trim: true
+    },
+    email: {
+      type: string,
+      required: 'Valid email address is required',
+      unique: true,
+      validate: {
+
+      }
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+)
+
+// friendCount virtual
+userSchema.virtual('friendCount').get(function(){
+  return this.friends.length
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
