1f80201a8b8bd26cccaf8470f653c836f045e16b
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-10-24 14:23:41 -0400

    also added next steps to user and thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+
+// get all thoughts
+// get one thought by id
+// post a thought (push thought id to user's thought array)
+// put a thought by id
+// delete a thought by id
+
+// api/thoughts/:id/reactions
+// post a reaction to a thought (add to thought's reaction array)
+// delete a reaction to a thought by reaction's id (remove from thought's reaction array)
+
 const {
   getCourses,
   getSingleCourse,
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+
+// get all users
+// get one user by id (and populate thoughts and friends arrays)
+// post a new user
+// put a user by id
+// delete a user by id (bonus to remove thoughts on user delete)
+
+// api/users/:userid/friends/:friendid
+// post to add friend to user's friend array
+// delete to remove friend from user's friend array
+
 const {
   getStudents,
   getSingleStudent,
